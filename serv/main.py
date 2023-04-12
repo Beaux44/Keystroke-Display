8eca30e2a524f678de7452eb13402ce2b09b7f7f
Beaux Moreau <beaux7312@gmail.com>
2023-04-11 21:34:42 -0500

    Added the overlay bit

--- serv/main.py
+++ serv/main.py
@@ -1,14 +1,44 @@
+from typing import Any, Dict, Optional
 from fastapi import FastAPI, WebSocket
+from fastapi.staticfiles import StaticFiles
+import bson, json
+from starlette import websockets
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+keybd_client: Optional[WebSocket] = None
+display_client: Optional[WebSocket] = None
 
-
-@app.websocket("/message-io-default")
-async def websocket_endpoint(websocket: WebSocket):
+@app.websocket("/keybd")
+async def keybd_ws(ws: WebSocket):
+    global keybd_client
     print("spaghet")
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_bytes()
-        await websocket.send_bytes(bytes(f"fk u {data.decode('ascii')}", 'ascii'))
-        print(f"received {data.decode('ascii')}")
+    await ws.accept()
+    keybd_client = ws
+    try:
+        while True:
+            data = bson.loads(await ws.receive_bytes())
+            if display_client is not None:
+                await display_client.send_json(data)
+    except websockets.WebSocketDisconnect:
+        if keybd_client is ws:
+            keybd_client = None
+        print("disconnected keyboard")
+
+
+@app.websocket("/display")
+async def display_ws(ws: WebSocket):
+    global display_client
+    await ws.accept()
+    display_client = ws
+    print("DISPLAY CONNECTED")
+
+    try:
+        while True:
+            data: Dict[str, Any] = await ws.receive_json()
+            print(f"Display sent {data}")
+    except websockets.WebSocketDisconnect:
+        if display_client is ws:
+            display_client = None
+        print("disconnected display")
 
---
+++ serv/static/display.js
@@ -0,0 +1,162 @@
+const socket = new WebSocket('ws://127.0.0.1:80/display');
+let keysQueue = [],
+    keysTimeout,
+    content;
+
+function getSymbol({ key, shift }) {
+    const SYMS = {
+        Numpad0: '0',
+        Numpad1: '1',
+        Numpad2: '2',
+        Numpad3: '3',
+        Numpad4: '4',
+        Numpad5: '5',
+        Numpad6: '6',
+        Numpad7: '7',
+        Numpad8: '8',
+        Numpad9: '9',
+        Numrow0: '0',
+        Numrow1: '1',
+        Numrow2: '2',
+        Numrow3: '3',
+        Numrow4: '4',
+        Numrow5: '5',
+        Numrow6: '6',
+        Numrow7: '7',
+        Numrow8: '8',
+        Numrow9: '9',
+        Backslash: '\\',
+        Slash: '/',
+        Comma: ',',
+        Period: '.',
+        Minus: '-',
+        Quote: '"',
+        Semicolon: ';',
+        LBracket: '[',
+        RBracket: ']',
+        Equal: '=',
+        Backquote: '`',
+        Enter: '↴',
+        Backspace: '⌫',
+        Space: '⎵',
+        Delete: '<Del>',
+    };
+
+    const SHIFT_SYMS = {
+        Numpad0: '0',
+        Numpad1: '1',
+        Numpad2: '2',
+        Numpad3: '3',
+        Numpad4: '4',
+        Numpad5: '5',
+        Numpad6: '6',
+        Numpad7: '7',
+        Numpad8: '8',
+        Numpad9: '9',
+        Numrow0: ')',
+        Numrow1: '!',
+        Numrow2: '@',
+        Numrow3: '#',
+        Numrow4: '$',
+        Numrow5: '%',
+        Numrow6: '^',
+        Numrow7: '&',
+        Numrow8: '*',
+        Numrow9: '(',
+        Backslash: '|',
+        Slash: '?',
+        Comma: '<',
+        Period: '>',
+        Minus: '_',
+        Quote: '\"',
+        Semicolon: ':',
+        LBracket: '{',
+        RBracket: '}',
+        Equal: '+',
+        Backquote: '~',
+        Enter: '↴',
+        Backspace: '⌫',
+        Space: '⎵',
+        Delete: '<Del>',
+    };
+
+    return shift ? SHIFT_SYMS[key] : SYMS[key];
+}
+
+function getKey({ key, ctrl, shift }) {
+    if(key.length === 1) {
+        key = shift ? key.toUpperCase() : key.toLowerCase();
+    } else {
+        key = getSymbol({ key, shift });
+    }
+
+    if(key === undefined)
+        return;
+
+    return ctrl ? `↑${key}` : key;
+}
+
+function shiftKeyQueue() {
+    keysQueue[0].remove();
+    keysQueue.shift();
+}
+
+function pushKeyPress(key) {
+    if(key === undefined)
+        return;
+
+    content.style.opacity = '100%';
+
+    const newEl = document.createElement("span");
+    newEl.innerText = key;
+    content.appendChild(newEl);
+
+    keysQueue.push(newEl);
+
+    if(keysQueue.length >= 24)
+        shiftKeyQueue();
+
+    if(keysTimeout !== undefined)
+        clearTimeout(keysTimeout);
+
+    keysTimeout = setTimeout(() => {
+        setTimeout(() => {
+            keysQueue.map((el) => {
+                el.remove()
+            });
+            content.style.transition = '';
+        }, 400);
+
+        keysTimeout = undefined;
+        requestAnimationFrame(() => {
+            content.style.transition = '';
+            content.style.opacity = '100%';
+            requestAnimationFrame(() => {
+                content.style.transition = 'opacity 0.4s';
+                content.style.opacity = '0%';
+            })
+        })
+    }, 2500);
+}
+
+socket.addEventListener('open', () => {
+    content = document.getElementById("display");
+    content.style.opacity = '0%';
+    console.log('WebSocket connection established');
+});
+
+
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+    switch(data.event) {
+        case 'KeyPress': {
+            if(!data.key.endsWith('Key'))
+                return;
+
+            data.key = data.key.slice(0, -3);
+            pushKeyPress(getKey(data));
+            break;
+        }
+    }
+});
+
