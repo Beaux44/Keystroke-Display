4bf6e7877f17907f9621a26396c31ba6d87cda92
Beaux Moreau <beaux7312@gmail.com>
2023-04-15 19:58:46 -0500

    Add ESLint

--- overlay/display.js
+++ overlay/display.js
@@ -10,7 +10,7 @@ const ALL_KEYS = {
     Numrow0: ['0', ')'], Numrow1: ['1', '!'], Numrow2: ['2', '@'], Numrow3: ['3', '#'], Numrow4: ['4', '$'],
     Numrow5: ['5', '%'], Numrow6: ['6', '^'], Numrow7: ['7', '&'], Numrow8: ['8', '*'], Numrow9: ['9', '('],
     Backslash: ['\\', '|'], Slash: ['/', '?'], Comma: [',', '<'], Period: ['.', '>'], Minus: ['-', '_'],
-    Quote: ['\'', '\"'], Semicolon: [';', ':'], LBracket: ['[', '{'], RBracket: [']', '}'], Equal: ['=', '+'],
+    Quote: ['\'', '"'], Semicolon: [';', ':'], LBracket: ['[', '{'], RBracket: [']', '}'], Equal: ['=', '+'],
     Backquote: ['`', '~'], Enter: ['↴', '↴'], Backspace: ['⌫', '⌫'], Space: ['⎵', '⎵'], Delete: ['⌦', '⌦'],
     Tab: ['⇥', '⇥'], Right: ['⤑', '⤑'], Left: ['⬸', '⬸'], Up: ['⇡', '⇡'], Down: ['⇣', '⇣'],
 };
@@ -62,13 +62,13 @@ function pushKeyPress(key) {
             requestAnimationFrame(() => {
                 content.style.transition = 'opacity 0.4s';
                 content.style.opacity = '0%';
-            })
-        })
+            });
+        });
     }, 2500);
 }
 
 socket.addEventListener('open', () => {
-    content = document.getElementById("display");
+    content = document.getElementById('display');
     content.style.opacity = '0%';
     console.log('WebSocket connection established');
 });
@@ -89,5 +89,5 @@ socket.addEventListener('message', async (event) => {
 });
 
 socket.addEventListener('close', () => {
-    document.getElementById("display").innerText = 'Websocket not connected';
+    document.getElementById('display').innerText = 'Websocket not connected';
 });
